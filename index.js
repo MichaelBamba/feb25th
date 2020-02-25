789effe00b2d7baf312738a0e279713ceac16a2a
MichaelBamba <mjbamba2@gmail.com>
2020-02-25 10:13:19 -0500

    first class work of the day

---
+++ index.js
@@ -0,0 +1,34 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    sayHello() {
+        console.log(`hi my name is ${this.name}`)
+    }
+    discribe() {
+        console.log(`${this.name} ${this.age} years old`)
+    }
+}
+
+
+const me = new Person('michael', 25);
+me.sayHello();
+me.discribe();
+
+class teacher extends Person {
+    constructor(name, age, subject) {
+        super(name, age);
+        this.subject = subject;
+
+    }
+
+    mySubject() {
+        console.log(`${this.name} teaches ${this.subject} at ${this.age}`)
+    }
+
+}
+
+const sean = new teacher(`sean`, 43, `javascript`)
+
+sean.mySubject();
