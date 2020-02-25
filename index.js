039d29021e7e0d3deaf94880505139d6616b6931
MichaelBamba <mjbamba2@gmail.com>
2020-02-25 10:31:56 -0500

    added and experimented with static methond

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ class Person {
     discribe() {
         console.log(`${this.name} ${this.age} years old`)
     }
+    static introduce(person) {
+        console.log(`this is ${person.name}, they are ${person.age} years old`)
+
+    }
 }
 
 
 const me = new Person('michael', 25);
+const jason = new Person('jason', 43)
 me.sayHello();
 me.discribe();
 
@@ -32,3 +37,5 @@ class teacher extends Person {
 const sean = new teacher(`sean`, 50, `javascript`)
 
 sean.mySubject();
+Person.introduce(jason);
+Person.introduce(sean);
